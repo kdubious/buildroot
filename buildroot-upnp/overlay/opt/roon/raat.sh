2220d489e7ff23f9274d8d30e34f06e2dd0c0400
Kevin Welsh <kwelsh@welshtechnologies.com>
2019-05-29 10:14:16 -0400

    fix the raat.mk file

--- buildroot-upnp/overlay/opt/roon/raat.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# The RAAT Loop
-while :
-do
-	/opt/roon/raat_app /opt/roon/config.json
-	sleep 2
-done
--- buildroot-upnp/package/raat/raat.mk
+++ buildroot-upnp/package/raat/raat.mk
@@ -4,29 +4,29 @@
 #
 ################################################################################
 
-RAAT_VERSION = v1.1
+RAAT_VERSION = v1.2
 #RAAT_SOURCE = raat-master.tar.gz
 RAAT_SITE = https://github.com/kdubious/raat
 RAAT_SITE_METHOD = git
-RAAT_LICENSE = GPL-3.0+
+RAAT_LICENSE = 
 RAAT_LICENSE_FILES = COPYING
 RAAT_INSTALL_STAGING = YES
 #RAAT_CONFIG_SCRIPTS = RAAT-config
-RAAT_DEPENDENCIES = dl libpthread-stubs
+#RAAT_DEPENDENCIES = dl libpthread-stubs
 
 define RAAT_BUILD_CMDS
-    $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) TARGET=linux-mp CONFIG=release all
+    $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) TARGET=linux-mp-buildroot CONFIG=release LD=$(TARGET_CC) all
 endef
 
-define RAAT_INSTALL_STAGING_CMDS
-    $(INSTALL) -D -m 0755 $(@D)/RAAT.a $(STAGING_DIR)/usr/lib/RAAT.a
-    $(INSTALL) -D -m 0644 $(@D)/foo.h $(STAGING_DIR)/usr/include/foo.h
-    $(INSTALL) -D -m 0755 $(@D)/RAAT.so* $(STAGING_DIR)/usr/lib
-endef
+#define RAAT_INSTALL_STAGING_CMDS
+#    $(INSTALL) -D -m 0755 $(@D)/RAAT.a $(STAGING_DIR)/usr/lib/RAAT.a
+#    $(INSTALL) -D -m 0644 $(@D)/foo.h $(STAGING_DIR)/usr/include/foo.h
+#    $(INSTALL) -D -m 0755 $(@D)/RAAT.so* $(STAGING_DIR)/usr/lib
+#endef
 
 define RAAT_INSTALL_TARGET_CMDS
-    $(INSTALL) -D -m 0755 $(@D)/RAAT.so* $(TARGET_DIR)/usr/lib
-    $(INSTALL) -d -m 0755 $(TARGET_DIR)/etc/foo.d
+	$(INSTALL) -d -m 0755 $(TARGET_DIR)/opt/roon
+    $(INSTALL) -D -m 0755 $(@D)/bin/release/linux/armv7hf/raat_app $(TARGET_DIR)/opt/roon/raat_app
 endef
 
 #define RAAT_USERS
