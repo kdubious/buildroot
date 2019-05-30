e5d8bc3f44144f98d221676110f3d5fb0c55c419
Kevin Welsh <kwelsh@welshtechnologies.com>
2019-05-30 19:05:07 -0400

    more updates

--- buildroot-upnp/post_build.sh
+++ buildroot-upnp/post_build.sh
@@ -1,2 +1,6 @@
 # LEAVE THIS for now, trying to deal with running Roon and MPD
-# rm -f $TARGET_DIR/etc/init.d/S95mpd
+rm -f $TARGET_DIR/etc/init.d/S95mpd
+echo `uuid -v4` > /opt/mp/uuid
+CONFIG = `jq --arg val $(cat uuid) '.deviceID=$val' $TARGET_DIR/opt/mp/config.json`;
+echo "" > $TARGET_DIR/opt/mp/config.json
+echo $CONFIG > $TARGET_DIR/opt/mp/config.json
