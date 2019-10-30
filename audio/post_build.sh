4d6f34033fb3cf3d8bc2089ebfe09c7db8a22d35
Kevin Welsh <kwelsh@welshtechnologies.com>
2019-10-29 20:58:30 -0400

    many updates for eMMC

--- audio/post_build.sh
+++ audio/post_build.sh
@@ -6,3 +6,21 @@ echo `uuid -v4` > $UUID
 CONFIG=`jq --arg val $(cat $UUID) '.deviceID=$val' $TARGET_DIR/opt/mp/config.json`;
 echo "" > $TARGET_DIR/opt/mp/config.json
 echo $CONFIG > $TARGET_DIR/opt/mp/config.json
+
+echo "pre tar the /opt folder"
+read -p "Press [Enter] key to continue..."
+tar -chJf $TARGET_DIR/data.tar.xz -C $TARGET_DIR/opt .
+read -p "Press [Enter] key to continue..."
+mv $TARGET_DIR/data.tar.xz $TARGET_DIR/../images/data.tar.xz
+read -p "Press [Enter] key to continue..."
+
+echo "pre kill $TARGET_DIR/opt"
+rm -r $TARGET_DIR/opt
+read -p "Press [Enter] key to continue..."
+
+echo "pre make new /opt folder"
+mkdir $TARGET_DIR/opt
+read -p "Press [Enter] key to continue..."
+
+
+echo '/dev/mmcblk1p3    /opt    ext4    defaults    0   0' >> $TARGET_DIR/etc/fstab
--- audio/post_image.sh
+++ audio/post_image.sh
@@ -1,2 +1,4 @@
 #rm $1/rootfs.tar
 #cp $1/* /mnt/d/toradex/buildroot/
+
+
