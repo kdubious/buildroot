06fb2571e7fe2833236253a7c7d3a2a6ef6fd916
Kevin Welsh <kwelsh@welshtechnologies.com>
2019-11-12 16:56:03 -0500

    updates

--- audio/overlay/sbin/automount.sh
+++ audio/overlay/sbin/automount.sh
@@ -12,18 +12,18 @@ if [ $mounted -ge 1 ]; then
                 exit 1
         fi
 
-        if ! rmdir "/mnt/usb/$1"; then
+        if ! rmdir "/mnt/MPD/USB/$1"; then
                 exit 1
         fi
-# not mounted, lets mount under /mnt/usb
+# not mounted, lets mount under /mnt/MPD/USB
 else
-        if ! mkdir -p "/mnt/usb/$1"; then
+        if ! mkdir -p "/mnt/MPD/USB/$1"; then
                 exit 1
         fi
 
-        if ! mount -o sync "/dev/$1" "/mnt/usb/$1"; then
+        if ! mount -o sync "/dev/$1" "/mnt/MPD/USB/$1"; then
                 # failed to mount, clean up mountpoint
-                if ! rmdir "/mnt/usb/$1"; then
+                if ! rmdir "/mnt/MPD/USB/$1"; then
                         exit 1
                 fi
                 exit 1
